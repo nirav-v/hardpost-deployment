15ee38a5f5f94b71dec5871ed424fc50c982f756
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-07-26 11:47:32 -0700

    revert to old vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,29 +1,17 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default ({ mode }) => {
-  const isProduction = mode === "production";
-
-  const proxyTarget = isProduction
-    ? "https://hardpost-02998f60f4cf.herokuapp.com/"
-    : "http://localhost:3000"; // Change the port to match your local backend port
-
-  return defineConfig({
-    plugins: [react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: proxyTarget,
-          changeOrigin: true,
-          secure: isProduction,
-        },
-        "/shop": {
-          target: proxyTarget,
-          changeOrigin: true,
-          secure: isProduction,
-        },
-      },
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://hardpost-02998f60f4cf.herokuapp.com/",
+        changeOrigin: true,
+        secure: true,
+      }, // Replace with your backend server URL
+      "/shop": "https://hardpost-02998f60f4cf.herokuapp.com/",
     },
-  });
-};
+  },
+});
