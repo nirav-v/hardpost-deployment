d1b11277ef93e84d6fadb9f626e8cea790709f36
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-07-26 10:36:45 -0700

    update proxy url in vite config to the  deployed heroku url

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": "https://hardpost-02998f60f4cf.herokuapp.com/", // Replace with your backend server URL
+      "/shop": "https://hardpost-02998f60f4cf.herokuapp.com/",
+    },
+  },
+});
