ad95d0ef8f3b755ef7da77bb0c1160fd8f346461
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-07-26 18:35:41 -0700

    remove all bs vite docs said to do, just gonna serve client on its own heroku separate heroku url and proxy to the sepratealy hosted sever

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,16 +5,6 @@ import react from "@vitejs/plugin-react";
 export default ({ mode }) => {
   return defineConfig({
     plugins: [react()],
-    build: {
-      manifest: true,
-      rollupOptions: {
-        input: "./src/main.jsx",
-      },
-    },
-    // server: {
-    //   origin: "http://localhost",
-    //   port: 3000,
-    // },
     server: {
       proxy: {
         "/api": "https://gentle-spire-83185-d5ea8d952a7d.herokuapp.com",
