57a702c06b3339324dca1297ad009cc1fe2f63d2
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-08-02 14:22:39 -0700

    update login fetch url

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -7,8 +7,7 @@ function LoginForm({ loggedIn, setLoggedIn }) {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const url =
-      "https://gentle-spire-83185-d5ea8d952a7d.herokuapp.com/api/user/login";
+    const url = "/api/user/login";
     const body = JSON.stringify({
       email: email,
       password: password,
